8be9d858bd7d4abdc26eedcc3795157b283ad3db
= <=>
2022-08-09 02:47:35 +0530

    added a text file for demo

--- src/routes/route.js
+++ src/routes/route.js
@@ -10,29 +10,52 @@ router.get('/test-me', function (req, res) {
     res.send('My second ever api!')
 });
 
-router.get('/students', function (req, res){
-    let students = ['Sabiha', 'Neha', 'Akash']
-    res.send(students)
+// problem 1
+router.get('/movies', function (req, res){
+    const movies = ['Rang de basanti','yeh jawaani hai diwaani','fanaa','avengar']
+    res.send(movies)
 })
+// promblem 2
+router.get('/movies/:indexNumber', function(req, res){
+    const movies = ['Rang de basanti','yeh jawaani hai diwaani','fanaa','avengar']
+    let request = req.params
+    let index = request.indexNumber
+    console.log('movies',index)
+    res.send('movies')
+   
+    })
+//promblem 3
+router.get('/films', function (req, res){
+    const films = [{
+ 'id': 1,
+ 'name': 'The Shining'
+}, {
+ 'id': 2,
+ 'name': 'Incendies'
+}, {
+ 'id': 3,
+ 'name': 'Rang de Basanti'
+}, {
+ 'id': 4,
+ 'name': 'Finding Nemo'
+}]
+res.send(films)
+})
+router.get('/films-details/:name', function (req, res){
+let value = req.params
+console.log("This is the request "+ JSON.stringify(requestParams))
+let filmNames = value.name
+console.log(filmNames)
+    res.send('hi')
 
-router.get('/student-details/:name', function(req, res){
-    /*
-    params is an attribute inside request that contains 
-    dynamic values.
-    This value comes from the request url in the form of an 
-    object where key is the variable defined in code 
-    and value is what is sent in the request
-    */
+ })
 
-    let requestParams = req.params
-
-    // JSON strigify function helps to print an entire object
-    // We can use any ways to print an object in Javascript, JSON stringify is one of them
-    console.log("This is the request "+ JSON.stringify(requestParams))
-    let studentName = requestParams.name
-    console.log('Name of the student is ', studentName)
-    
-    res.send('Dummy response')
+ // promblem 5
+ router.get('/flims/:filmid', function(req, res){
+ if (valve === req.params)}
+      { return value; {
+    else
+    } console.log('no such valve')
 })
 
 module.exports = router;
