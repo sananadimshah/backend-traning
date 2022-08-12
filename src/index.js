b24dda8427b770acd2850b156d220a930fac096d
= <=>
2022-08-12 21:58:50 +0530

    added

--- src/index.js
+++ src/index.js
@@ -1,10 +1,23 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const route = require('./routes/route.js');
+const { default: mongoose } = require('mongoose');
 const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
+mongoose.connect("mongodb+srv://functionupsana:99zr3eE9dRQ62e8a@cluster0.1d0h0gc.mongodb.net/functionupsana?retryWrites=true&w=majority"
+,{
+   useNewUrlParser: true
+}
+).then( () => {console.log( "MongoDb is connected")}  )
+.catch( err => console.log(err))
+
+
+
+
+
 app.use('/', route);
 
 app.listen(process.env.PORT || 3000, function() {
