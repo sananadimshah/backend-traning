12394c9be2b5ce82d85f5949f92a0c2f80d93591
= <=>
2022-08-14 21:26:45 +0530

    added

--- src/routes/route.js
+++ src/routes/route.js
@@ -33,6 +33,28 @@ router.get('/student-details/:name', function(req, res){
     console.log('Name of the student is ', studentName)
     
     res.send('Dummy response')
+
+    //  Q1.problem
+// -write an api which gives the missing number in an array of integers starting from 1….e.g [1,2,3,5,6,7] : 4 is missing
+// Your route code will look like this
+router.get("/sol1", function (req, res) {
+    //logic : sum of numbers is n(n+1)/2..so get sum of all numbers in array. now take sum of numbers till last digit in the array
+    let arr = [1, 2, 3, 5, 6, 7]
+    let sum1 = 0;
+    for (let i = 0; i < arr.length; i++) {
+        sum1 = sum1 + arr[i]
+    }
+    let n = arr.pop();
+    let sum2 = (n * (n + 1)) / 2;
+
+    let missingNumber = sum2 - sum1;
+    console.log(missingNumber);
+
+
+
+
+    res.send({ data: missingNumber });
+});
 })
 
 module.exports = router;
