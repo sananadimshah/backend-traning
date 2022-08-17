e25a95dae138ed43b3c5e29890817f33718470bf
= <=>
2022-08-17 13:53:08 +0530

    added

--- src/controllers/bookController.js
+++ src/controllers/bookController.js
@@ -1,16 +1,50 @@
+
 const BookModel= require("../models/bookModel")
 
 const createBook= async function (req, res) {
     let data= req.body
-
     let savedData= await BookModel.create(data)
     res.send({msg: savedData})
 }
 
-const getBooksData= async function (req, res) {
-    let allBooks= await BookModel.find(  { authorName : "SK" , isPublished: true }  )
-    res.send({msg: allBooks})
+const allbookList= async function (req, res) {
+        let allBooks= await BookModel.find()
+        res.send({msg:allBooks})
 }
 
+const BookList= async function (req, res) {
+        let data= req.body
+        let Book= await BookModel.find(data).select({ bookname : 1,author : 1, _id:0})
+        res.send({msg:Book})
+}
+const getBooksInYear= async function (req, res) {
+        year = req.query.year
+        let BooksInYear= await BookModel.find({year:{$eq:year}})
+        res.send({msg:BooksInYear })
+}
+const getParticularBooks=async function (req, res) {
+        let data= req.body
+        let ParticularBooks= await BookModel.find(data)
+        res.send({msg: ParticularBooks})
+}
+const getXINRBooks= async function (req, res) {
+        let  XINRBooks= await BookModel.find({priceindianPrice:{$in :["100INR","200INR","500INR"]}})
+        res.send({msg:XINRBooks })
+}
+const getRandomBooks= async function (req, res) {
+        let RandomBooks = await BookModel.find($0r[{stockAvailable :true},{pages :{$gt : 500}}])
+        res.send({msg: RandomBooks})
+}
+
+
+
+
+
+
 module.exports.createBook= createBook
-module.exports.getBooksData= getBooksData
+module.exports.allbookList=allbookList
+module.exports.BookList= BookList
+module.exports.getBooksInYear= getBooksInYear
+module.exports.getParticularBooks= getParticularBooks
+module.exports.getXINRBooks= getXINRBooks
+module.exports.getRandomBooks= getRandomBooks
--- src/models/bookModel.js
+++ src/models/bookModel.js
@@ -1,20 +1,24 @@
 const mongoose = require('mongoose');
 
-const bookSchema = new mongoose.Schema( {
-    bookName: String, 
-    authorName: String, 
-    tags: [String],
-    
-    isPublished: Boolean,
+const bookSchema = new mongoose.Schema({
+    bookName: {
+        type: String,
+        required: true,
+    },
     prices: {
         indianPrice: String,
         europePrice: String,
     },
-    sales: {type: Number, default: 10}
+    year: { type: Number, default: 2021 },
+    tags: [{
+        authorName: String,
+        totalPages: String,
+        stockAvailable: Boolean
+    }]
 }, { timestamps: true });
 
 
-module.exports = mongoose.model('Book', bookSchema) //users
+module.exports = mongoose.model('Bookmodel', bookSchema) //users
 
 //Validation:
 //require:true
