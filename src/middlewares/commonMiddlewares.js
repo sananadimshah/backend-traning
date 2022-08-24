f68e5855d02351f05bc8157178c050027ca7efcd
= <=>
2022-08-24 23:37:14 +0530

    added

--- src/middlewares/commonMiddlewares.js
+++ src/middlewares/commonMiddlewares.js
@@ -1,8 +1,8 @@
 
 const mid1= function ( req, res, next) {
-    req.falana= "hi there. i am adding something new to the req object"
-    console.log("Hi I am a middleware named Mid1")
-    next()
+    // req.falana= "hi there. i am adding something new to the req object"
+    console.log("current",req.OrignalUrl)
+    next();
 }
 
 const mid2= function ( req, res, next) {
---
+++ src/routes/tempCodeRunnerFile.js
@@ -0,0 +1,3 @@
+
+router.post("/createUser", UserController.createUser  )
+router.get("/getUsersData", UserController.getUsersData)
