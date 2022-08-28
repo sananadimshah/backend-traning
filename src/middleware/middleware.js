18fbbe8a8fa51ad3afd79b6a2209469296509510
= <=>
2022-08-28 23:16:47 +0530

    added

--- src/middleware/middleware.js
+++ src/middleware/middleware.js
@@ -1,1 +1,12 @@
-const mid1 = function (req,res,next)
+
+const mid1 = function (req, res, next) {
+    let token = req.headers.x-auth-token
+    if (!token) {
+        res.send({ msg: "The request is missing a mandatory header" })
+    }
+    else {
+        req.isFreeAppUser = token;
+        next()
+    }
+}
+module.exports.mid1 = mid1
