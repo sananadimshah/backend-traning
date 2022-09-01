b7c649b6b28cbc037fee4c6df763572036c100d8
= <=>
2022-09-01 23:26:40 +0530

    Added

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -4,61 +4,102 @@ const userModel = require("../models/userModel");
 /*
   Read all the comments multiple times to understand why we are doing what we are doing in login api and getUserData api
 */
+//======================================Create User================================//
 const createUser = async function (req, res) {
-  let data = req.body;
-  let savedData = await userModel.create(data);
-  res.send({ msg: savedData });
-};
+    try {
+        let data = req.body;
+        let savedData = await userModel.create(data);
+        res.status(201).send({ msg: savedData });
+    }
+    catch (err) {
+        console.log("This is the error:", err.message)
+        res.status(400).send({ msg: "BAD REQUEST" })
 
+    }
+}
+//======================================loginUser=============================//
 const login = async function (req, res) {
-  let userName = req.body.emailId;
-  let password = req.body.password;
-  let user = await userModel.findOne({ emailId: userName, password: password });
-  if (!user)
-    return res.send({
-      status: false,
-      msg: "username or the password is not corerct",
-    });
-    
-  let token = jwt.sign(
-    {
-      userId: user._id.toString(),
-      batch: "thorium",
-      organisation: "FunctionUp",
-    },
-    "functionup-plutonium-very-very-secret"
-  );
-  res.setHeader("x-auth-token", token);
-  res.send({ status: true, token: token });
-};
+    try {
+        let userName = req.body.emailId;
+        let password = req.body.password;
+        let user = await userModel.findOne({ emailId: userName, password: password });
+        if (!user)
+            return res.status(400).send({
+                status: false,
+                msg: "username or the password is not corerct",
+            });
+
+        let token = jwt.sign(
+            {
+                userId: user._id.toString(),
+                batch: "thorium",
+                organisation: "FunctionUp",
+            },
+            "functionup-plutonium-very-very-secret"
+        );
+        res.setHeader("x-auth-token", token);
+        res.status(201).send({ status: true, token: token });
+    }
+    catch (err) {
+        console.log("This is the error:", err.message)
+        res.status(500).send({ msg: "BAD REQUEST" })
+    }
+}
+//========================================getUserData===========================//
+
 
 const getUserData = async function (req, res) {
+    try {
+        let userId = req.params.userId;
+        let userDetails = await userModel.findById(userId);
+        if (!userDetails) {
+            return res.status(404).send({ status: false, msg: "No such user exists" });
+        }
+        else
 
-  let userId = req.params.userId;
-  let userDetails = await userModel.findById(userId);
-  if (!userDetails)
-    return res.send({ status: false, msg: "No such user exists" });
+            res.status(201).send({ status: true, data: userDetails });
+    }
+    catch (err) {
+        console.log("This is the error:", err.message)
+        res.status(500).send({ msg: "BAD REQUEST" })
+    }
+}
+//=========================================updateUser===========================//
 
-  res.send({ status: true, data: userDetails });
-};
+
 const updateUser = async function (req, res) {
-  let userId = req.params.userId;
-  let user = await userModel.findById(userId);
+    try {
+        let userId = req.params.userId;
+        let user = await userModel.findById(userId);
 
-  if (!user) {
-    return res.send("No such user exists");
-  }
+        if (!user) {
+            return res.status(404).send("No such user exists");
+        }
 
-  let userData = req.body;
-  let updatedUser = await userModel.findOneAndUpdate({ _id: userId }, userData);
-  res.send({ status: updatedUser, data: updatedUser });
-};
+        let userData = req.body;
+        let updatedUser = await userModel.findOneAndUpdate({ _id: userId }, userData);
+        res.status(201).send({ status: updatedUser, data: updatedUser });
+    }
+    catch (err) {
+        console.log("This is the error:", err.message)
+        res.status(500).send({ msg: "BAD REQUEST" })
+    }
+}
+//===========================================Delected=============================//
+
 
 const isDeleted = async function (req, res) {
-  let userId = req.params.userId;
-  let updatedUser = await userModel.findOneAndUpdate({ _id: userId }, { isDeleted: true }, { new: true })
-  res.send({ status: true, data: updatedUser });
+    try {
+        let userId = req.params.userId;
+        let updatedUser = await userModel.findOneAndUpdate({ _id: userId }, { isDeleted: true }, { new: true })
+        res.status(201).send({ status: true, data: updatedUser });
+    }
+    catch (err) {
+        console.log("This is the error:", err.message)
+        res.status(400).send({ msg: "BAD REQUEST" })
+    }
 }
+
 module.exports.createUser = createUser;
 module.exports.getUserData = getUserData;
 module.exports.updateUser = updateUser;
--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -2,23 +2,29 @@
 const jwt = require("jsonwebtoken");
 
 const auth = function (req, res, next) {
+try {
   let token = req.headers["x-Auth-token"];
   if (!token) token = req.headers["x-auth-token"];
-
-  if (!token) return res.send({ status: false, msg: "token must be present" });
-
+  
+  if (!token) return res.status(499).send({ status: false, msg: "token must be present" });
+  
   console.log(token);
   let decodedToken = jwt.verify(token, "functionup-plutonium-very-very-secret");
   if (!decodedToken) {
-    return res.send({ status: false, msg: "token is invalid" });
+    return res.status(400).send({ status: false, msg: "token is invalid" });
   }
   let loginUser = decodedToken.userId
   let checkuser = req.params.userId
   if (loginUser !== checkuser)
-    res.send({ msg: "invalid User permission denied" })
-
+    res.status(403).send({ msg: "invalid User permission denied" })
+  
   next()
+  }
+ catch (error) {
+  console.log("This is the error:", err.message)
+  res.status(500).send({ msg: "BAD REQUEST" })
+}
 }
 
-module.exports.auth = auth
 
+module.exports.auth = auth
